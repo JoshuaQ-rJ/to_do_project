7bcf2625e865f51884749115eaeeb49e6246e689
Joshua Gonzalez <joshuagonza2610@gmail.com>
2026-03-19 17:17:11 -0500

    Created task models

---
+++ src/models.py
@@ -0,0 +1,7 @@
+def create_task_model(title, description, priority):
+    return {
+        "tittle": title,
+        "description": description,
+        "priority": priority,
+        "status": "to do"
+        }
