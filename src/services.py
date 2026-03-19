c3bc806f31f1efc499ad036e7ceb9a03aa655523
joshua <joshua.quintero@outlook.com>
2026-03-19 13:09:58 -0500

    service limpio

--- src/services.py
+++ src/services.py
@@ -6,15 +6,11 @@ def create_task():
     status="por hacer"
         
     tasks.append({"title":title,"description":description,"priority":priority,"status":status})    
-    for i in tasks:
-        print(f"the task {i["title"]} it was crated correctly")
-    return tasks
+    print(f"task {tasks["title"]} it was crated correctly")    
 def show_task():
-    print("\n---tasks---")
-    for i,t in enumerate(tasks):
-        print(f"{i+1}. {t["title"]}\nthe description are : {t["description"]}\nwith priority: {t["priority"]} and status: {t["status"]}")
-
-
-
-print(create_task())
-print(show_task())
+    if tasks==None:
+        print('tasks are empty, please crate one')
+    else:    
+        print("\n---tasks---")
+        for i,t in enumerate(tasks):
+            print(f"{i+1}. {t['title']}\nthe description are : {t['description']}\nwith priority: {t['priority']} and status: {t['status']}")
