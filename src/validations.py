c332673b86cb0343f3930ceef41b64ae39a94b23
Jose Romero <jrlara4739@gmail.com>
2026-03-19 19:22:37 -0500

    Update validations.py

--- src/validations.py
+++ src/validations.py
@@ -3,24 +3,30 @@ def validate_int(prompt, min_value= None, max_value = None):
         value = input(prompt)
         if not value.isdigit():
             print("incorrect number")
-            value = int(value)
-            if min_value is not None and  value < min_value:
+            continue
+        value = int(value)
+        if min_value is not None and  value < min_value:
                 print(f"Number must be at least {min_value}.")
-                if max_value is not None and value < min_value:
-                    print(f"Number must be at most {max_value}.")
-                    return value
-def validate_non_empty_string(promt):
+                continue
+        if max_value is not None and value < min_value:
+                print(f"Number must be at most {max_value}.")
+                continue
+        return value
+        
+def validate_non_empty_string(prompt):
     while True:
-        value = input(promt).strip()
+        value = input(prompt).strip()
         if not value:
             print("This field cannot be empty.")
-            return value
+            continue
+        return value
         
 def validate_priority(promt):
     valid_priorities = ["high", "mediun", "low"]
     while True:
-        value = input(promt).strip().lower()
+        value = input(promtp).strip().lower()
         if value not in  valid_priorities:
             print(f"Invalid pririty. chooose from : {','.join(valid_priorities)}.")
-            return value       
+            continue
+        return value       
         
